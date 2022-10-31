7a5e93e65879964cc985169c4fa5b0762b87e7ef
Nick Straw <nick@broxholme.com>
2022-10-31 13:49:11 +0000

    Update to lts-19.30

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# AOC2017
+# AOC2015
+
+Just as finished, not polished, some days will be slow.
---
+++ g.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git push https://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@github.com/b1g3ar5/AOC2015.git
